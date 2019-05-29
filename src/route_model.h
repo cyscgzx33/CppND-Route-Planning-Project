34f0bcba0b1019f3614750af856e6270648aa970
cyscgzx33 <chenyus@umich.edu>
2019-05-29 11:48:52 -0400

    feat: set up the system

--- src/route_model.h
+++ src/route_model.h
@@ -15,7 +15,12 @@ class RouteModel : public Model {
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+        Node* parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0;
+        bool visited = false;
+        std::vector<Node*> neighbors;
+
       private:
         // Add private Node variables and methods here.
         int index;
@@ -25,8 +30,10 @@ class RouteModel : public Model {
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    std::vector<Node>& SNodes() { return m_Nodes; }
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
 
 };
